c6b52f3585203bc6ab7c1ee1ec5103bb4f65197d
Vaqif <taqiyevv937@gmail.com>
2023-04-06 21:36:59 +0400

    [MASTER] first onboarding screen are completed

--- app/src/main/java/com/vagif_tagiyev/newsfinalapp/ui/splashscreen/SplashScreenFragment.kt
+++ app/src/main/java/com/vagif_tagiyev/newsfinalapp/ui/splashscreen/SplashScreenFragment.kt
@@ -5,18 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.vagif_tagiyev.newsfinalapp.R
 import com.vagif_tagiyev.newsfinalapp.databinding.FragmentSplashScreenBinding
+import kotlinx.coroutines.*
 
 class SplashScreenFragment : Fragment() {
-    private lateinit var splashScreenBinding : FragmentSplashScreenBinding
+    private lateinit var splashScreenBinding: FragmentSplashScreenBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val splashBinding = FragmentSplashScreenBinding.inflate(layoutInflater,container,false)
+    ): View {
+        val splashBinding = FragmentSplashScreenBinding.inflate(layoutInflater, container, false)
+
+        val scope = CoroutineScope(Dispatchers.Main)
+
+        scope.launch {
+            delay(3000L)
+            splashToViewPager()
+        }
 
         return splashBinding.root
+    }
+
+    private fun splashToViewPager() {
+        val action = SplashScreenFragmentDirections.splashViewpagerScreen()
+        findNavController().navigate(action)
     }
 
 }
