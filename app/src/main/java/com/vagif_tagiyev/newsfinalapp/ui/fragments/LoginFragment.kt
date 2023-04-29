3b6a8705c35503449a71589d1effae7e648156ee
Vaqif <taqiyevv937@gmail.com>
2023-04-29 15:00:11 +0400

    [MASTER] This is my first example of app

--- app/src/main/java/com/vagif_tagiyev/newsfinalapp/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/vagif_tagiyev/newsfinalapp/ui/fragments/LoginFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.Toast
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -28,6 +29,13 @@ class LoginFragment : Fragment() {
         //Initialize Firebase Authentication
         auth = Firebase.auth
 
+        //val currentUser = auth.currentUser
+
+/*
+        if (currentUser != null){
+            Navigation.findNavController(loginBinding.root).navigate(R.id.login_account)
+        }
+*/
 
         loginBinding.signUpBtn.setOnClickListener {
             val email = loginBinding.emailEditText.text.toString()
@@ -46,6 +54,25 @@ class LoginFragment : Fragment() {
 
 
         }
+
+
+/*
+        loginBinding.signInBtn.setOnClickListener {
+            val email = loginBinding.emailEditText.text.toString()
+            val password = loginBinding.passwordEditText.text.toString()
+
+            if(email == "" || password == ""){
+                Toast.makeText(context,"E-mail və şifrə daxil edin." ,Toast.LENGTH_LONG).show()
+            }else{
+                auth.signInWithEmailAndPassword(email, password).addOnSuccessListener{
+                    val signAcc = LoginFragmentDirections.loginAccount(email,password)
+                    findNavController().navigate(signAcc)
+                }.addOnFailureListener {
+                    Toast.makeText(context,it.localizedMessage ,Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+*/
 
 
 
